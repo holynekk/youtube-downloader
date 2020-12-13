fe16b12b81140e89543d8708be8848658bb6b872
HolyNekK <e238109@metu.edu.tr>
2020-12-14 02:46:18 +0300

    src_downloader.py updated

--- src-downloader.py
+++ src-downloader.py
@@ -1,9 +1,39 @@
 from pytube import YouTube
 
-yt_link = input("Enter the video link: ")
 
-yt_obj = YouTube(yt_link)
+while(True):
+    yt_link = input("Enter the video link: ")
 
-for stream in yt_obj:
-    print(stream)
+    yt_obj = YouTube(yt_link)
 
+    # Title of the video
+    print("Title: ", yt_obj.title)
+
+    # View count of the video
+    print("View count: ", yt_obj.views)
+
+    # Length of the video
+    print("Length: ", yt_obj.length)
+
+    print("Is this the video that you want to download?[y/n]: ",end="")
+    print("'q' for quit.")
+    yes_no = input()
+
+    if(yes_no in 'qqqQQQ'):
+        print("Ok then, see ya!")
+        break
+
+    if(yes_no):
+
+        ys = yt_obj.streams.get_highest_resolution()
+
+        print("Donwloading...")
+        ys.download("./../../Videos")
+        print("Download completed.")
+
+        print("want to continue?[y/n]: ")
+        tr = input()
+        if(tr == 'n'):
+            break
+    else:
+        print("The link is wrong, Somebody jebaited you!")
